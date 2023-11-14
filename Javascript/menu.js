8d41f9ab29dc7cfc139d058203d374664853185e
Muhammad Hasnat <mhasnat647@gmail.com>
2023-11-14 22:49:29 +0500

    navbar update

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 const topBar = `
 <div class="container-fluid px-3">
 <div class="d-flex align-items-center justify-content-center">
@@ -22,6 +22,153 @@ const topBar = `
 
 const pageTopBar = document.getElementById("pageTopBar");
 pageTopBar.insertAdjacentHTML("beforeend", topBar);
+*/
+const menu = `
+<div class="container-fluid sticky-top bg-white main-navbar">
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid px-md-5 py-2">
+    <a class="navbar-brand" href="index.html">
+      <!-- <img src="logo/logo-color.jpg" loading="lazy" id="main-logo" /> -->
+      PHONEFIXESSEX
+    </a>
+
+    <button
+      class="navbar-toggle d-sm-inline-block d-md-none d-lg-none border-0 bg-transparent"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasScrolling"
+      aria-controls="offcanvasScrolling"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="1.3em"
+        viewBox="0 0 448 512"
+      >
+        <path
+          d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+        />
+      </svg>
+    </button>
+
+    <div
+      class="collapse navbar-collapse justify-content-end"
+      id="navbarSupportedContent"
+    >
+      <ul
+        class="navbar-nav mb-2 mb-lg-0 fs-6 fw-bold color-main font-roboto gap-5 align-items-center"
+      >
+        <li class="nav-item">
+          <a
+            class="nav-link p-0 mb-0 d-inline-block"
+            aria-current="page"
+            href="/index.html"
+            >Home</a
+          >
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link p-0 mb-0 d-inline-block"
+            href="#devices-options"
+            >Devices</a
+          >
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link p-0 mb-0 d-inline-block"
+            href="#review-carousal"
+            >Testimonials</a
+          >
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link p-0 mb-0 d-inline-block"
+            href="#why-fonefixessex"
+            >Why Us?</a
+          >
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link p-0 mb-0 d-inline-block" href="faq.html"
+            >FAQ</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+<div
+class="offcanvas offcanvas-start"
+data-bs-scroll="true"
+data-bs-backdrop="false"
+tabindex="-1"
+id="offcanvasScrolling"
+aria-labelledby="offcanvasScrollingLabel"
+>
+<div class="offcanvas-header">
+  <a class="navbar-brand" href="index.html">
+    PHONEFIXESSEX
+    <!-- <img src="logo/logo-color.jpg" loading="lazy" id="main-logo" /> -->
+  </a>
+  <button
+    type="button"
+    class="btn-close"
+    data-bs-dismiss="offcanvas"
+    aria-label="Close"
+  ></button>
+</div>
+<div class="col-11 mx-auto">
+  <hr />
+</div>
+<div class="offcanvas-body">
+  <div class="justify-content-eb">
+    <ul
+      class="navbar-nav mb-2 mb-lg-0 fs-6 fw-bold color-main font-works gap-4 align-items-center"
+    >
+      <li class="nav-item">
+        <a
+          class="nav-link p-0 mb-0 d-inline-block"
+          aria-current="page"
+          href="/index.html"
+          >Home</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link p-0 mb-0 d-inline-block"
+          href="#devices-options"
+          >Devices</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link p-0 mb-0 d-inline-block"
+          href="#review-carousal"
+          >Testimonials</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link p-0 mb-0 d-inline-block"
+          href="#why-fonefixessex"
+          >Why Us?</a
+        >
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link p-0 mb-0 d-inline-block" href="faq.html"
+          >FAQ</a
+        >
+      </li>
+    </ul>
+  </div>
+</div>
+</div>
+`;
+
+const pageMenu = document.getElementById("pageMenu");
+pageMenu.insertAdjacentHTML("beforeend", menu);
 
 const footer = `
 <div class="container py-3 mb-5">
